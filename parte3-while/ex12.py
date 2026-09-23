249baccf1490683aa7ada0b69d03300a9c0dc685
Guilherme Maia <guilherme_me_silva@estudante.sesisenai.org.br>
2026-09-23 17:48:34 -0300

    add exercicio 12

---
+++ parte3-while/ex12.py
@@ -0,0 +1,6 @@
+soma = 0 
+numero = int(input("Digite um número: "))
+while numero !=0:
+    soma = soma + numero
+    numero = int(input("Digite um número: "))
+    print("A soma dos números digitados é:", soma)
