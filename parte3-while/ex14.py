5b5b82442c957ff3eb405f2e9f006173e3d00253
Guilherme Maia <guilherme_me_silva@estudante.sesisenai.org.br>
2026-09-23 17:53:53 -0300

    add ex 14

---
+++ parte3-while/ex14.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número: "))
+
+contador = 1
+
+while contador <= 10:
+    resultado = numero * contador
+    print(numero, "x", contador, "=", resultado)
+    contador = contador + 1
